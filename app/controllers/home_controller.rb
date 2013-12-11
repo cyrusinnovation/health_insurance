f4b5fb51241a26e2b2cb225e44fe418bf43e9c30
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-12-11 18:01:32 -0500

    fixed not showing myrsc for first time users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    current_user.myrsc = Myrsc.new if current_user.myrsc.nil?
     current_user.oxfords.build
   end
 
