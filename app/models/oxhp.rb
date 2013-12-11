759c85446660614fcfd2fe322f5ea1e42b9d4067
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-12-11 12:58:41 -0500

    refactoring oxhp and making it so you can pass in the eob to get

--- app/models/oxhp.rb
+++ app/models/oxhp.rb
@@ -12,32 +12,33 @@ class Oxhp
     login
     goto_claims_page
     show_all_claims
-    first_eob
+    get_eob 1
   end
 
-  def first_eob
-    first_row = @all_claims_page.search('#claims_sum_tbl tr:nth-child(2)')
-    first_row.search('td:nth-child(2)').first.content =~ /Service: (\d+\/\d+\/\d+)/
-    service_date = $1
-    service_code = get_service_from_cell(first_row.search('td:nth-child(2)').first.content)
-    deductible_amount = first_row.search('td:nth-child(5)').first.content.delete("$")
-    link = @all_claims_page.links_with(:text => 'More Details')[0]
-    link.href =~ /'(\/Member.*)'/
-    url = "#{domain}#{$1}"
+  def get_eob eob_number
+    row = @all_claims_page.search("#claims_sum_tbl tr:nth-child(#{eob_number + 1})")
+    service_date = get_service_date row
+    service_code = get_service_code row
+    deductible_amount = get_deductible_amount row
 
-    link.instance_variable_set(:@href, url)
-    link.instance_variable_set(:@uri, nil)
-    claim_detail_page = link.click
+    goto_claim_detail_page eob_number
 
-    form = claim_detail_page.form('claimsSummaryForm')
-    file = @agent.submit(form, form.buttons.first)
-    filename = "/Users/jacobodonnell/programming/health_insurance/tmp/#{file.filename}"
-    file.save filename # or .body
+    claim_number = get_claim_number
+    filename = get_filename
     [Claim.new(service_date, service_code, deductible_amount, @claimant, @relationship, filename)]
   end
 
   def goto_claims_page
     @claims_page = @user_home_page.links.find { |l| l.text == 'Claims & Accounts' }.click
+  end
+
+  def goto_claim_detail_page eob_number
+    link = @all_claims_page.links_with(:text => 'More Details')[eob_number - 1]
+    link.href =~ /'(\/Member.*)'/
+    url = "#{domain}#{$1}"
+
+    link.instance_variable_set(:@href, url)
+    @claim_detail_page = link.click
   end
 
   def show_all_claims
@@ -65,6 +66,33 @@ class Oxhp
   private
   def get_service_from_cell content
     return 'Lab' if content =~ /LB\d+/
-    return 'Medical Office Visit'
+    return 'Medical Office Visit' if content =~ /P\d+/
+    return "NOT UNDERSTAND CODE #{content}"
   end
+
+  def get_service_code row
+    get_service_from_cell(row.search('td:nth-child(2)').first.content)
+  end
+
+  def get_service_date row
+    row.search('td:nth-child(2)').first.content =~ /Service: (\d+\/\d+\/\d+)/
+    $1
+  end
+
+  def get_deductible_amount row
+    row.search('td:nth-child(5)').first.content.delete("$")
+  end
+
+  def get_claim_number
+    @claim_detail_page.search('label[text()="Claim Number:"]').first.next_sibling.next_sibling.content
+  end
+
+  def get_filename
+    form = @claim_detail_page.form('claimsSummaryForm')
+    file = @agent.submit(form, form.buttons.first)
+    filename = "/Users/jacobodonnell/programming/health_insurance/tmp/#{file.filename}"
+    file.save filename
+    filename
+  end
+
 end
