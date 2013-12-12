f3603c2bc1bc167d93cb37cc34b7731d7fb68833
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-12-12 01:22:07 -0500

    added canceling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,12 @@ class HomeController < ApplicationController
     current_user.oxfords.build
   end
 
+  def cancel
+    claim = current.user.oxfords.where(claim_number: params[:claim_number])
+    mark_as_canceled if claim
+    redirect_to root_path
+  end
+
   def submit_info
     current_user.update_attributes(params.require(:user).permit(myrsc_attributes: [ :username, :password, :id ], oxfords_attributes: [ :username, :password, :id, :relationship, :claimant]))
 
--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,8 +1,19 @@
 class Claim < ActiveRecord::Base
   belongs_to :oxford
 
-  def mark_as_processed
-    self.state = 'processed'
+  def self.ready_for_submitting
+    Claim.where("created_at < ?", 1.day.ago).where(state: 'unprocessed')
+  end
+
+  def mark_as_submitted
+    self.state = 'submitted'
+    # delete file
+    save
+  end
+
+  def mark_as_canceled
+    self.state = 'canceled'
+    # delete file
     save
   end
 
