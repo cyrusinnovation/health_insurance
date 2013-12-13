a24a27e410e21dc1866897dceed688f4589655f5
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-12-13 11:21:22 -0500

    added some loggin

--- app/models/submit_eobs.rb
+++ app/models/submit_eobs.rb
@@ -1,6 +1,7 @@
 class SubmitEobs
   def initialize
     Claim.ready_for_submitting.each do |claim|
+      puts "claim #{claim.claim_number}"
       uploader = ClaimUploader.new
       uploader.submit_claim claim
       claim.mark_as_submitted
