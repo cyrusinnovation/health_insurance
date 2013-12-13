63e1b493f23519607749d1c38eea4ce9e654429f
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-12-13 11:14:46 -0500

    added sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ HealthInsurance::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { :host => 'health-insurance-oca.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
