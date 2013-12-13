f29379a80167ff5218735ceb7442de71b652f206
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-12-13 11:21:07 -0500

    added flash messages to confirm actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,11 +9,11 @@ class HomeController < ApplicationController
   def cancel
     claim = Claim.where(claim_number: params[:claim_number]).first
     claim.mark_as_canceled if claim and claim.ready_for_processing? and claim.user == current_user
-    redirect_to root_path
+    redirect_to root_path, notice: 'Cancelled'
   end
 
   def submit_info
     current_user.update_attributes(params.require(:user).permit(myrsc_attributes: [ :username, :password, :id ], oxfords_attributes: [ :username, :password, :id, :relationship, :claimant]))
-    redirect_to root_path
+    redirect_to root_path, notice: 'Submitted'
   end
 end
