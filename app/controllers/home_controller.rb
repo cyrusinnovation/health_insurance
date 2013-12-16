db44a3f0bcd531b205efee16913139c5493b5dad
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-12-16 18:41:12 -0500

    fixed another problem!!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    current_user.myrsc = Myrsc.new if current_user.myrsc.nil?
+    current_user.build_myrsc if current_user.myrsc.nil?
     current_user.oxfords.build
   end
 
@@ -14,7 +14,7 @@ class HomeController < ApplicationController
 
   def submit_info
     current_user.update_attributes(params.require(:user).permit(myrsc_attributes: [ :username, :password, :id ], oxfords_attributes: [ :username, :password, :id, :relationship, :claimant]))
-    current_user.myrsc = Myrsc.new if current_user.myrsc.nil?
+    current_user.build_myrsc if current_user.myrsc.nil?
     current_user.oxfords.build
 
     render :index
